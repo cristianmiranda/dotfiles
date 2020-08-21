ded7843f72b4aebcd79c21a51c8d9e4ed8f62517
Cristian Miranda <crism60@gmail.com>
2020-08-21 17:05:56 -0300

    install[bat]: cat on steroids

--- home/.fzf.bash
+++ home/.fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/cmiranda/dotfiles/home/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/cmiranda/dotfiles/home/fzf/bin"
+if [[ ! "$PATH" == */home/cmiranda/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/cmiranda/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/cmiranda/dotfiles/home/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/cmiranda/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/cmiranda/dotfiles/home/fzf/shell/key-bindings.bash"
+source "/home/cmiranda/fzf/shell/key-bindings.bash"
