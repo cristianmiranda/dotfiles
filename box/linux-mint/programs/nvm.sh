8c0290d94ea46cf968ebe3adfe142ff58f96cbf5
Cristian Miranda <crism60@gmail.com>
2020-08-25 19:59:44 -0300

    install[nodejs]: Latest nodejs & npm

--- box/linux-mint/programs/nvm.sh
+++ box/linux-mint/programs/nvm.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# NVM (Node + npm)
-curl -so- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
-# nvm install node # Needs to be executed manually
-mkdir -p ~/.config/configstore
-sudo chown -R $(whoami) ~/.config/configstore
-
-# Load NVM and install node and npm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
-nvm install node
+sudo apt-get install -y nodejs
