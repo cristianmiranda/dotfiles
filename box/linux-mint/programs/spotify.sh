737187e5dfe18c97c2e8d5557a0832f8fa84b920
Cristian Miranda <crism60@gmail.com>
2020-09-06 16:36:32 -0300

    install[spotify]: Replaced snap version with latest from source

---
+++ box/linux-mint/programs/spotify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update
+sudo apt-get install -y spotify-client
