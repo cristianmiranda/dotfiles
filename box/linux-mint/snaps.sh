ea2d80e50ac607b3029d4df923effd9f76d38f17
Cristian Miranda <crism60@gmail.com>
2020-09-06 15:40:56 -0300

    install[postman]: Replaced snap version with latest from source

--- box/linux-mint/snaps.sh
+++ box/linux-mint/snaps.sh
@@ -12,7 +12,6 @@ fi
 snapInstall chromium
 snapInstall code --classic
 snapInstall intellij-idea-ultimate --classic
-snapInstall postman
 snapInstall slack --classic
 snapInstall spotify
 snapInstall telegram-desktop
