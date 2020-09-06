8cf8866eee471831ccee79b6d3b4108ed8bdd867
Cristian Miranda <crism60@gmail.com>
2020-09-06 19:39:22 -0300

    install[vscode]: Replaced snap version with latest from source

---
+++ box/linux-mint/programs/vscode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
+sudo install -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y code # or code-insiders
+
