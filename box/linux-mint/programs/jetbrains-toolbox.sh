c4c8c76055bca42ecb50eee4a2ff0cd60006bf4b
Cristian Miranda <crism60@gmail.com>
2020-09-07 01:22:12 -0300

    install[jetbrains]: Replaced snap version with latest from source

---
+++ box/linux-mint/programs/jetbrains-toolbox.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /tmp
+wget -cO jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
+tar -xzf jetbrains-toolbox.tar.gz
+
+DIR=$(find . -maxdepth 1 -type d -name jetbrains-toolbox-\* -print | head -n1)
+cd $DIR
+./jetbrains-toolbox
+cd ..
+rm -r $DIR
+rm jetbrains-toolbox.tar.gz
