9cf092c69b1537b71c13f10fa7ec16f1101d27a9
Cristian Miranda <crism60@gmail.com>
2020-09-10 23:54:10 -0300

    config[lid]: Run custom action on open/close
    - On close: Mute volume & i3lock
    - On open: Unmute volume

--- box/linux-mbp/custom-config/lid.sh
+++ box/linux-mbp/custom-config/lid.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
  
 USER=cmiranda
- 
+
+#This runs so that root can run the following command under the user's environment
+source /home/$USER/.Xdbus
+
 grep -q close /proc/acpi/button/lid/*/state
  
 if [ $? = 0 ]; then
   su -c  "sleep 1 && xset -display :0.0 dpms force off" - $USER
+  /home/$USER/bin/close_lid
 fi
- 
+
 grep -q open /proc/acpi/button/lid/*/state
  
 if [ $? = 0 ]; then
   su -c  "xset -display :0 dpms force on &> /tmp/screen.lid" - $USER
+  /home/$USER/bin/open_lid
 fi
