af4859319b5e952397cf5f92c215a8ac6406162e
Cristian Miranda <crism60@gmail.com>
2020-10-03 13:28:31 -0300

    :building_construction: set-wallpaper: macOS support

--- scripts/utils/config.py
+++ scripts/utils/config.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import platform
 import yaml
 
 
 def getConfig():
     # home = os.popen("echo $HOME").read().strip()
     # Absolute home path so that root can use this
-    home = "/home/cmiranda"
+    if platform.system() == "Linux":
+        home = "/home/cmiranda"
+    else:
+        home = "/Users/cmiranda"
 
     with open("{}/dotfiles/config.yaml".format(home)) as f:
         return yaml.load(f, Loader=yaml.FullLoader)
