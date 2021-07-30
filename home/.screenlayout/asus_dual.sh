07e6d73266e5b1ca8c8e796f53d0a068c799ed56
Cristian Miranda <crism60@gmail.com>
2021-07-30 14:32:07 -0300

    :wrench: displays: Add support for ASUS dual monitor

---
+++ home/.screenlayout/asus_dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-0 --off --output DP-2 --off
