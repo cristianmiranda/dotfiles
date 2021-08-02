9de081221bf2d985150af2228e1cc9fac926d932
Cristian Miranda <crism60@gmail.com>
2021-08-02 17:27:45 -0300

    :bug: dotfiles: Fix arch dependencies

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-yay -Syu
+sudo pacman -Syu
+sudo pacman --noconfirm -S go gcc
 
-yay -S git-secret figlet lolcat
+git clone https://aur.archlinux.org/yay.git ~/yay
+cd ~/yay
+makepkg -i
+cd -
 
-pip3 list --format=columns | grep setuptools || pip3 install setuptools
-pip3 list --format=columns | grep click || pip3 install click
-pip3 list --format=columns | grep PyYAML || pip3 install PyYAML
+yay -Syyu
+yay -S python-pip git-secret figlet lolcat
+
+pip list --format=columns | grep setuptools || pip install setuptools
+pip list --format=columns | grep click || pip install click
+pip list --format=columns | grep PyYAML || pip install PyYAML
