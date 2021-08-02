3a2cc8947c52215f2d119a275e4bf8a26b0da8f6
Cristian Miranda <crism60@gmail.com>
2021-08-02 20:46:52 -0300

    :wrench: dotfiles: Use --no-confirm for package upgrades

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo pacman -Syu
+sudo pacman --noconfirm -Syu
 sudo pacman --noconfirm -S go gcc
 
 git clone https://aur.archlinux.org/yay.git ~/yay
 cd ~/yay
-makepkg -i
+makepkg --noconfirm -i
 cd -
 
-yay -Syyu
-yay -S python-pip git-secret figlet lolcat
+yay --noconfirm -Syyu
+yay --noconfirm -S python-pip git-secret figlet lolcat
 
 pip list --format=columns | grep setuptools || pip install setuptools
 pip list --format=columns | grep click || pip install click
