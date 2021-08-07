b9840a5d1cb20c8422155a76a530a970a40a2fe3
Cristian Miranda <crism60@gmail.com>
2021-08-07 19:52:39 -0300

    :zap: nvidia: Install optimus-manager

--- home/.screenlayout/asus_dual.sh
+++ home/.screenlayout/asus_dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-0 --off --output DP-2 --off
+xrandr --output DP-0 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-0 --off --output eDP-1-1 --off
