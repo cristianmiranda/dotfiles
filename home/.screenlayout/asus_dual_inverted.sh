293419073bd36aeaf3e14f3bb72427f3d818db82
Cristian Miranda <crism60@gmail.com>
2021-08-14 13:18:23 -0300

    :bug: screenlayout: Fix monitors position
    Left monitor used to take space from the right one

--- home/.screenlayout/asus_dual_inverted.sh
+++ home/.screenlayout/asus_dual_inverted.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --primary --mode 2560x1440 --pos 2552x0 --rotate normal --output HDMI-0 --off --output eDP-1-1 --off
+xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-0 --off --output eDP-1-1 --off
