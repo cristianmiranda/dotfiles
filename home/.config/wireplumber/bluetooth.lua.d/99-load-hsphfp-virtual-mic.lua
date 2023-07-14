856973f5db4e2ff7fb609ce5ef1aefa6c7f83d56
Cristian Miranda <crism60@gmail.com>
2023-07-14 10:44:40 -0300

    :children_crossing: audio: Disable automatic bluez profile switching

--- home/.config/wireplumber/bluetooth.lua.d/99-load-hsphfp-virtual-mic.lua
+++ home/.config/wireplumber/bluetooth.lua.d/99-load-hsphfp-virtual-mic.lua
@@ -1,1 +1,6 @@
-load_script("wp-virtual-hsphfp-mic.lua", {profile_debounce_time_ms = 4000})
+--
+-- This module automatically switches bluetooth profiles depending on streams
+-- It's not working as expected, so I disabled it for now. Manual switch is more reliable.
+--
+
+-- load_script("wp-virtual-hsphfp-mic.lua", {profile_debounce_time_ms = 4000})
