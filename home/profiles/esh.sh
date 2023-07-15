9fe5ccb7c91d412181058e33086cfe469be30b9c
Cristian Miranda <crism60@gmail.com>
2023-07-15 16:51:41 -0300

    :wrench: esh: Add common and specific aliases

--- home/profiles/esh.sh
+++ home/profiles/esh.sh
@@ -1,3 +1,45 @@
 #!/usr/bin/env bash
 
+#
+# Installation: Stick it in .bashrc or .bash_profile
+#
+# PROFILE_URL=https://raw.githubusercontent.com/cristianmiranda/dotfiles/master/home/profiles/esh.sh
+# source <(curl -H 'Cache-Control: no-cache' -s $PROFILE_URL)
+#
+
+#
+# Fixes vim colors on Alacritty
+#
 export TERM=xterm-256color
+
+#
+# ESH Specific Aliases
+#
+alias sutomcat="sudo /bin/su - tomcat -s /bin/bash"
+alias suescribe="sudo su - escribe"
+
+#
+# ESH Specific Exports
+#
+export PATH=$PATH:/opt/escribe/bin
+
+#
+# Common Aliases
+#
+alias ll="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias l="ls -CF"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias grep="grep --color=auto"
+alias c="clear"
+alias h="history"
+
+#
+# Common Exports
+#
+export EDITOR=vim
+export VISUAL=vim
