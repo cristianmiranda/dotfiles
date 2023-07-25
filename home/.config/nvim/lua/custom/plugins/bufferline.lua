284543e538f54a6f69a82bf1f927183249fe57d2
Cristian Miranda <crism60@gmail.com>
2023-07-25 08:50:19 -0300

    :fire: nvim: delete kickstart config

--- home/.config/nvim/lua/custom/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "akinsho/bufferline.nvim",
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    vim.opt.termguicolors = true
-    require("bufferline").setup{}
-  end,
-}
-
