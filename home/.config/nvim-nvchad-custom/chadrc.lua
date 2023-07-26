187b86e1bd08cc9cab17efcd00dcb7eaed863954
Cristian Miranda <crism60@gmail.com>
2023-07-26 10:04:58 -0300

    :wrench: nvim: add custom config for nvchad

---
+++ home/.config/nvim-nvchad-custom/chadrc.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.ui = {
+	theme_toggle = { "onedark", "onedark_latte" },
+	theme = "onedark",
+	transparency = true,
+}
+
+M.plugins = require "custom.plugins"
+M.mappings = require "custom.mappings"
+
+return M
+
---
+++ home/.config/nvim-nvchad-custom/init.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt
+
+opt.autoindent = true
+opt.smartindent = true
+
