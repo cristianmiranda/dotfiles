717c564f34c5c83de601da8a3d9bdb0464f23f83
Cristian Miranda <crism60@gmail.com>
2023-08-01 21:27:24 -0300

    :fire: box: remove linux-mint

--- box/linux-mint/programs/firefox-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ls /usr/local/firefox_dev && echo 'Firefox DEV already installed ' && exit 0 || echo 'Installing firefox DEV'
- 
-cd /tmp
-
-curl --location "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" | tar --extract --verbose --preserve-permissions --bzip2
-
-sudo mv firefox /usr/local/firefox_dev
--- box/linux-mint/programs/postman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ls /usr/local/postman && echo 'Postman already installed ' && exit 0 || echo 'Installing Postman'
- 
-cd /tmp
-
-curl --location "https://dl.pstmn.io/download/latest/linux64" | tar -z --extract --verbose --preserve-permissions
-
-sudo mv Postman /usr/local/postman
