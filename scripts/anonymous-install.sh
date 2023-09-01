c985400e490ab9c8e8a2a89aa1f143610e977e5f
Cristian Miranda <crism60@gmail.com>
2023-09-01 19:40:08 -0300

    :bug: dotbot[anonymous]: clone repo before sync

--- scripts/anonymous-install.sh
+++ scripts/anonymous-install.sh
@@ -4,6 +4,8 @@
 echo "source ~/dotfiles/home/profiles/anonymous.sh" >> ~/.bash_profile
 
 # Dotbot sync
+cd ~/dotfiles
+git clone git@github.com:anishathalye/dotbot.git || wget -O dotbot.zip https://github.com/anishathalye/dotbot/archive/refs/heads/master.zip && unzip dotbot.zip && mv dotbot-master dotbot && rm dotbot.zip
 bash ~/dotfiles/dotbot/bin/dotbot -v -c ~/dotfiles/home/dotbot.anonymous.conf.yaml
 
 # Apply config
