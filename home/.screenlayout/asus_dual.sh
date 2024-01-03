40e50b5fdb394c35e9b635172eee083bc97e5d27
Cristian Miranda <crism60@gmail.com>
2024-01-03 00:00:34 -0300

    :wrench: screenlayout: update asus display scripts

--- home/.screenlayout/asus_dual.sh
+++ home/.screenlayout/asus_dual.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-
-LAPTOP_DISPLAY=`xrandr --query | grep "eDP" | awk '{print $1}'`
-
-xrandr --output DP-0 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-0 --off --output $LAPTOP_DISPLAY --off
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --mode 2560x1440 --pos 0x0 --rotate normal
