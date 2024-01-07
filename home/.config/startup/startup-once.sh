4b71a589be4dee340c37f4e7c60916d19a6bdd31
Cristian Miranda <crism60@gmail.com>
2024-01-07 12:43:57 -0300

    :wrench: startup: rearranged startup apps

--- home/.config/startup/startup-once.sh
+++ home/.config/startup/startup-once.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-( eval `ssh-agent -s` & )
+#
+# Set display
+#
 ( $HOME/bin/set-display dual & )
+
+#
+# System
+#
+( eval `ssh-agent -s` & )
 ( bluetoothctl power on & )
 ( setxkbmap es & )
 ( numlockx & )
 ( $HOME/bin/tmu --init & )
+( docker & )
+( xset -b & )
+
+#
+# Apps
+#
+( 1password --silent & )
+( nm-applet & )
+( blueman-applet & )
+( pasystray & )
+( alttab -d 1 -w 1 -i 100x100 -t 100x100 -bg "#222D31" -frame "#1793d1" -s 2 & )
