a5b26a87e15d56462d12bf6e314d1245e0c73d9c
Cristian Miranda <crism60@gmail.com>
2024-02-08 11:48:20 -0300

    :wrench: sdkman: upgrade to java 21

--- box/arch/programs/sdkman.sh
+++ box/arch/programs/sdkman.sh
@@ -2,7 +2,7 @@
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 11.0.9.hs-adpt
+sdk install java 21.0.2-amzn
 sdk install ant
 sdk install maven 3.8.8
 sdk install mvnd 0.7.1
