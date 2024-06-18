f4c634b879b89e8a53ae2655475cb8febc3f8c2a
Cristian Miranda <crism60@gmail.com>
2024-06-18 10:52:44 -0300

    :sparkles: sdkman[java]: 21.0.2-amzn => 21.0.2-graal

--- box/arch/programs/sdkman.sh
+++ box/arch/programs/sdkman.sh
@@ -2,7 +2,7 @@
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 21.0.2-amzn
+sdk install java 21.0.2-graal
 sdk install ant
 sdk install maven 3.8.8
 sdk install mvnd 0.7.1
