90cb95ac57205c5b3af8d23c942fff2c9c2b84fd
Cristian Miranda <crism60@gmail.com>
2024-11-08 19:43:20 -0300

    :sparkles: displays: add 3 and 4 monitors layout

---
+++ home/.screenlayout/four_asus.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 5120x813 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 7680x0 --rotate left --output DisplayPort-2 --mode 2560x1440 --pos 2560x813 --rotate normal --output HDMI-A-0 --mode 2560x1440 --pos 0x813 --rotate normal
