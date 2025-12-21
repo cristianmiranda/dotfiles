2c35bc926862d81c98292b2e8e637bc3911b79a7
Cristian Miranda <crism60@gmail.com>
2025-12-21 02:45:20 +0000

    ✨ ubuntu: add gh cli to programs

---
+++ box/ubuntu/programs/gh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ">>> Installing gh..."
+
+if command -v gh &> /dev/null; then
+    echo ">>> Already installed: gh"
+    exit 0
+fi
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install -y gh
+
+echo ">>> gh installed successfully"
